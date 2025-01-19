418b919b8efce50d58a298b7d556228f52257593
mayurk224 <mayurkamble0250@gmail.com>
2025-01-19 19:19:47 +0530

    getting student info dynamically from url using @PathVariable annotation

--- springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -2,6 +2,7 @@ package net.javaguides.springboot.controller;
 
 import net.javaguides.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -27,5 +28,12 @@ public class StudentController {
         students.add(new Student(3, "ramesh", "Kamble"));
         students.add(new Student(4, "suresh", "Kamble"));
         return students;
+    }
+
+    @GetMapping("students/{id}/{first-name}/{last-name}")
+    public Student studentPathVariable(@PathVariable int id,
+                                       @PathVariable("first-name") String firstName,
+                                       @PathVariable("last-name") String lastName){
+        return new Student(id, firstName, lastName);
     }
 }
