77d208cba587d010245fe2c768d6fe6233e1df0f
mayurk224 <mayurkamble0250@gmail.com>
2025-01-21 12:29:48 +0530

    setup project configure mysql database, added entity class and added repository

---
+++ banking-app/src/main/java/com/javaguide/banking/entity/Account.java
@@ -0,0 +1,21 @@
+package com.javaguide.banking.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "accounts")
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "account_holder_name")
+    private String accountHolderName;
+    private double balance;
+}
