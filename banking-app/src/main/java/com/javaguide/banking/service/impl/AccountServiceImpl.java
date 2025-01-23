256cb62251e087dd44402f7de47b927ee8436a72
mayurk224 <mayurkamble0250@gmail.com>
2025-01-23 09:41:48 +0530

    delete user by id

--- banking-app/src/main/java/com/javaguide/banking/service/impl/AccountServiceImpl.java
+++ banking-app/src/main/java/com/javaguide/banking/service/impl/AccountServiceImpl.java
@@ -64,4 +64,10 @@ public class AccountServiceImpl implements AccountService {
                 .map(account -> AccountMapper.mapToAccountDto(account))
                 .collect(Collectors.toList());
     }
+
+    @Override
+    public void deleteAccount(Long id) {
+        Account account = accountRepository.findById(id).orElseThrow(()-> new RuntimeException("Account does not exist"));
+        accountRepository.deleteById(id);
+    }
 }
