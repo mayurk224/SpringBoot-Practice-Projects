431afa044d58723bd3e529e33c0a4c6f4226f51a
mayurk224 <mayurkamble0250@gmail.com>
2025-01-25 21:39:04 +0530

    created category jpa entity added some packages

---
+++ expense-tracker-app/src/main/java/com/expensetracker/expense/entity/Category.java
@@ -0,0 +1,23 @@
+package com.expensetracker.expense.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String name;
+}
