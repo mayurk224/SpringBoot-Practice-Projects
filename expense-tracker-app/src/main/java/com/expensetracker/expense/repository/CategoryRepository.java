e42cf26dbe59dd5ad8e5038d7fa34caf673732bb
mayurk224 <mayurkamble0250@gmail.com>
2025-01-25 21:42:08 +0530

    created CategoryRepository interface

---
+++ expense-tracker-app/src/main/java/com/expensetracker/expense/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.expensetracker.expense.repository;
+
+import com.expensetracker.expense.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
