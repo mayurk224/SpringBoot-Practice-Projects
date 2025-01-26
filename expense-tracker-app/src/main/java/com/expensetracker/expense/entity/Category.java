ad5e270741ac8bd14b1a9e192b051f6d7656c31a
mayurk224 <mayurkamble0250@gmail.com>
2025-01-26 14:28:12 +0530

    added Expense entity class

--- expense-tracker-app/src/main/java/com/expensetracker/expense/entity/Category.java
+++ expense-tracker-app/src/main/java/com/expensetracker/expense/entity/Category.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "category")
+@Table(name = "categories")
 public class Category {
 
     @Id
---
+++ expense-tracker-app/src/main/java/com/expensetracker/expense/entity/Expense.java
@@ -0,0 +1,33 @@
+package com.expensetracker.expense.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="expenses")
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private LocalDateTime expenseDate;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+}
