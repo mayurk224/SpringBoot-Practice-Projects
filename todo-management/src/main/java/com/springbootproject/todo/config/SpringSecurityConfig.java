42ea4ae452dac1e44bb3c81b40ac0593ed2a94f3
mayurk224 <mayurkamble0250@gmail.com>
2025-01-29 19:36:37 +0530

    configured basic authentication

---
+++ todo-management/src/main/java/com/springbootproject/todo/config/SpringSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.springbootproject.todo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable()) // Updated way to disable CSRF
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+}
