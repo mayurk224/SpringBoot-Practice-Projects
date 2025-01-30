41db2d74779d0b46283c738bfbba6db883e3daf7
mayurk224 <mayurkamble0250@gmail.com>
2025-01-30 09:34:30 +0530

    created userrepository and rolerepository

---
+++ todo-management/src/main/java/com/springbootproject/todo/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.springbootproject.todo.repository;
+
+import com.springbootproject.todo.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+}
