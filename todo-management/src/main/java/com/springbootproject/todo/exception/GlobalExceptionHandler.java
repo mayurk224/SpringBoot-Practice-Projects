cf8fdede99b20ac992667288ba28e0b581beba67
mayurk224 <mayurkamble0250@gmail.com>
2025-01-30 20:37:13 +0530

    handled excpetion while register with same details with errordetails and globalexception class

---
+++ todo-management/src/main/java/com/springbootproject/todo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.springbootproject.todo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler{
+
+    @ExceptionHandler(TodoAPIException.class)
+    public ResponseEntity<ErrorDetails> handleTodoAPIException(TodoAPIException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(errorDetails,HttpStatus.BAD_REQUEST);
+    }
+}
