5578741d8a77856cffe451acb342a00710ee1314
mayurk224 <mayurkamble0250@gmail.com>
2025-01-30 10:12:04 +0530

    created customuserdetailsservice class

---
+++ todo-management/src/main/java/com/springbootproject/todo/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.springbootproject.todo.security;
+
+import com.springbootproject.todo.entity.User;
+import com.springbootproject.todo.exception.ResourceNotFoundException;
+import com.springbootproject.todo.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail).orElseThrow(
+                ()-> new UsernameNotFoundException("User not exist by username or email")
+        );
+
+        Set<GrantedAuthority> authorities = user.getRoles().stream().map((role)->new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+        return new org.springframework.security.core.userdetails.User(
+                usernameOrEmail,
+                user.getPassword(),
+                authorities
+        );
+    }
+}
