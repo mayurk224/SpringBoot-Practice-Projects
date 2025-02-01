f4589a2e98fc020449420a07cd080d00905b49b1
mayurk224 <mayurkamble0250@gmail.com>
2025-02-01 09:33:05 +0530

    created jwttokenprovider class

---
+++ todo-management/src/main/java/com/springbootproject/todo/security/JwtTokenProvider.java
@@ -0,0 +1,59 @@
+package com.springbootproject.todo.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt.expiration-milliseconds}")
+    private long jwtExpirationDate;
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expirationDate = new Date(currentDate.getTime() + jwtExpirationDate);
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expirationDate)
+                .signWith(key())
+                .compact();
+        return token;
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(
+                Base64.getDecoder().decode(jwtSecret)
+        );
+    }
+
+    public String getUsername(String token){
+         Claims claims = Jwts.parser()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token){
+        Jwts.parser()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token);
+        return true;
+    }
+}
