793ceca422d5be5748da28befee13a116e2cd2c5
mayurk224 <mayurkamble0250@gmail.com>
2025-02-04 14:14:47 +0530

    create handler method to take response from form and save it to database

--- student-management-system/src/main/java/com/springproject/sms/controller/StudentController.java
+++ student-management-system/src/main/java/com/springproject/sms/controller/StudentController.java
@@ -5,6 +5,8 @@ import com.springproject.sms.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -29,5 +31,12 @@ public class StudentController {
         StudentDto studentDto = new StudentDto();
         model.addAttribute("student", studentDto);
         return "create_student";
+    }
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute("student") StudentDto studentDto){
+        // Save the student and redirect to the list of students
+        studentService.createStudent(studentDto);
+        return "redirect:/students";
     }
 }
--- student-management-system/src/main/java/com/springproject/sms/service/impl/StudentServiceImpl.java
+++ student-management-system/src/main/java/com/springproject/sms/service/impl/StudentServiceImpl.java
@@ -28,4 +28,10 @@ public class StudentServiceImpl implements StudentService {
                 .collect(Collectors.toList());
         return studentDtos;
     }
+
+    @Override
+    public void createStudent(StudentDto studentDto) {
+        Student student= StudentMapper.mapToStudent(studentDto);
+        studentRepository.save(student);
+    }
 }
