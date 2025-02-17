477e441e6bb4d03605c93d6786e5429580485085
mayurk224 <mayurkamble0250@gmail.com>
2025-02-17 19:31:01 +0530

    learned noargcontructor allarg and requiredarg annotation

--- lombok-tutorial/src/main/java/com/springproject/Test.java
+++ lombok-tutorial/src/main/java/com/springproject/Test.java
@@ -1,18 +1,24 @@
 package com.springproject;
 
 public class Test {
+//    public static void main(String[] args) {
+//        User user = new User();
+//        user.setFirstName("Mayur");
+//        user.setLastName("Kamble");
+//        user.setEmail("kamble@gmail.com");
+//        user.setAge(25);
+//        user.setPhone("9860896312");
+//
+//        System.out.println("First Name: " + user.getFirstName());
+//        System.out.println("Last Name: " + user.getLastName());
+//        System.out.println("Email: " + user.getEmail());
+//        System.out.println("Age: " + user.getAge());
+//        System.out.println("Phone: " + user.getPhone());
+//    }
+
     public static void main(String[] args) {
         User user = new User();
-        user.setFirstName("Mayur");
-        user.setLastName("Kamble");
-        user.setEmail("kamble@gmail.com");
-        user.setAge(25);
-        user.setPhone("9860896312");
-
-        System.out.println("First Name: " + user.getFirstName());
-        System.out.println("Last Name: " + user.getLastName());
-        System.out.println("Email: " + user.getEmail());
-        System.out.println("Age: " + user.getAge());
-        System.out.println("Phone: " + user.getPhone());
+        User user1 = new User(1,"mayur","kamble","Mayur@gmail.com",25,"9860896318");
+        User user2 = new User("Mayur","Kamble");
     }
 }
--- lombok-tutorial/src/main/java/com/springproject/User.java
+++ lombok-tutorial/src/main/java/com/springproject/User.java
@@ -1,11 +1,12 @@
 package com.springproject;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+//@Getter
+//@Setter
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class User {
 
 //    @Getter
@@ -14,12 +15,31 @@ public class User {
 
 //    @Getter
 //    @Setter
-    private String firstName;
+    private final String firstName;
 
 //    @Getter(AccessLevel.PROTECTED)
 //    @Setter(AccessLevel.PROTECTED)
-    private String lastName;
+    private final String lastName;
     private String email;
     private int age;
     private String phone;
+
+//    public User() {
+//        this.firstName = "";
+//        this.lastName = "";
+//    }
+//
+//    public User(int id, String firstName, String lastName, String email, int age, String phone) {
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.email = email;
+//        this.age = age;
+//        this.phone = phone;
+//    }
+//
+//    public User(String firstName, String lastName) {
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//    }
 }
