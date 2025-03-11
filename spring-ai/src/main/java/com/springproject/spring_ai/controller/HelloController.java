86a7714ab6e4ba4c479f2b496e8d644ec765732b
mayurk224 <mayurkamble0250@gmail.com>
2025-03-11 08:30:08 +0530

    api with prompt template

--- spring-ai/src/main/java/com/springproject/spring_ai/controller/HelloController.java
+++ spring-ai/src/main/java/com/springproject/spring_ai/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.springproject.spring_ai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
 import java.util.Objects;
 
 @RestController
@@ -20,6 +23,28 @@ public class HelloController {
     public String prompt(@RequestParam String message) {
         return chatClient
                 .prompt(message)
+                .call()
+                .chatResponse()
+                .getResult()
+                .getOutput()
+                .getText();
+    }
+
+    @GetMapping("/celeb")
+    public String getCelebDetails(@RequestParam String name) {
+        String message = """
+                List the details of the Famous personality {name}
+                along with their career achivevements
+                show the details in the readable format
+                """;
+
+        PromptTemplate template = new PromptTemplate(message);
+        Prompt promt = template.create(
+                Map.of("name" ,name)
+        );
+
+        return chatClient
+                .prompt(promt)
                 .call()
                 .chatResponse()
                 .getResult()
